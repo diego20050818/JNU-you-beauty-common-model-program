6fc9b89f90d680b3e9a6c040b26c6ae9835fe0ce
diego20050818 <151654966+diego20050818@users.noreply.github.com>
2025-05-15 20:06:43 +0800

    添加用法脚本：usage.ipynb

--- 深圳杯C题/data_analyser.py
+++ 深圳杯C题/data_analyser.py
@@ -19,7 +19,35 @@ with open(nodes_file, 'r', encoding='utf-8') as f:
 
 graph = UndirectedGraph(nodes_info, edges_info)
 
-dg_nodes = graph.find_nodes_by_attribute("DG", True)
-print("带有分布式发电的节点:", dg_nodes)
+def rprint(info:str,result:any) -> None:
+    """
+    打印结果
+    :param info: 信息
+    :param result: 结果
+    :return: None
+    """
+    logger.info(info)
+    print(result)
+
+rprint("带有分布式发电的节点:", graph.find_nodes_by_attribute("DG", True))
+rprint("查找具有特定属性的边",graph.find_edges_by_attribute("type", "普通线"))
+rprint("查找两个节点之间的路径", graph.find_path(1, 59))
+rprint("获取所有节点的度数", graph.get_all_degrees())
+rprint("找出两个节点之间的所有路径", graph.get_all_paths(1, 5))
+rprint("获取属性的统计摘要", graph.get_attribute_summary("DG"))
+rprint("计算图的平均度数", graph.get_average_degree())
+rprint("查找两个节点之间的边", graph.get_edge(1, 2))
+rprint("获取边属性", graph.get_edge_attribute(1, 2))
+rprint("统计边特定属性的分布情况", graph.get_edge_attribute_statistics("type"))
+rprint("获取节点属性", graph.get_node_attribute(1))
+rprint("获取节点特定属性的分布情况", graph.get_node_attribute_statistics("DG"))
+rprint("获取节点的度", graph.get_node_degree(1))
+rprint("检查两个节点之间是否有边", graph.has_edge(1, 2))
+rprint("检查两个节点之间是否连通，并返回路径", graph.is_connected(1, 2))
+
+#可视化从起始节点到一个或者多个目标结点的最短路径
+graph.plot_shortest_paths(1, [2,62,4])
+
+
 
 graph.print_metrics_report()
